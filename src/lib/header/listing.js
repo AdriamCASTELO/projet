930371ee150a798540d78dc101612f5dcaec8e68
AdriamCASTELO <adriam.roberth.23@eigsi.fr>
2022-04-26 14:51:33 +0200

    Initial commit

---
+++ src/lib/header/listing.js
@@ -0,0 +1,28 @@
+const products = [
+  {product1: {
+    name: "Tenis Nike",
+    price: 15,
+    quantity: 5,
+    size: 'M',}
+  },
+  {
+    name: "Tenis adidas",
+    price: 17,
+    quantity:3,
+    size: 'P',
+  },
+  {
+    name: "Tenis converswe",
+    price: 18,
+    quantity: 4,
+    size: 'M',
+  },
+  {
+    name: "Tenis olympicus",
+    price: 14,
+    quantity: 5,
+    size: 'G',
+  },
+];
+
+export default products;
